9b59ad8884fc8f2d2c656a33601a76f7dc37b827
RicardoCartaxo <joaocartaxo95@gmail.com>
2019-11-26 16:34:46 +0100

    Started the cache

--- MicroProcesseur/c_memcache.vhd
+++ MicroProcesseur/c_memcache.vhd
@@ -30,12 +30,32 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity c_memcache is
+	PORT(clk, write_read_cpu, cache_activation: IN STD_LOGIC;
+		 data_cpu_entry: IN STD_LOGIC_VECTOR(7 downto 0);
+		 data_cpu_exit: OUT STD_LOGIC_VECTOR(7 downto 0);
+		 address_cpu: IN STD_LOGIC_VECTOR(15 downto 0);
+		 ack_cpu : OUT STD_LOGIC;
+		);
 end c_memcache;
 
 architecture Behavioral of c_memcache is
+	type MEMORY is array (integer range <>) of STD_LOGIC_VECTOR(7 downto 0);
+	type ADDRESSE is array (integer range <>) of STD_LOGIC_VECTOR(15 downto 0);
+		SIGNAL cache_data : MEMORY(0 to 15);
+		SIGNAL cache_addr : ADRESSE(0 to 15);
 
-begin
 
+begin -- processes
+
+    write : PROCESS(clk) BEGIN
+            IF clk'event and clk = '1' then  
+                IF write_read_cpu = '1' AND cache_activation = '1' then 
+                   for I in 0 to 15 loop
+						if address_cpu = cache_addr(I) THEN
+						end if;
+                end if;
+            end if;
+    end PROCESS;
 
 end Behavioral;
 
